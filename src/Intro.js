0dc75b84140a754a7c81e391b9a5222a3325b1be
22abhirajput <89356711+22abhirajput@users.noreply.github.com>
2022-10-03 15:06:50 +0530

    first commit

---
+++ src/Intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Intro() {
+    return (
+        <div style={{textAlign:"center",color:"Blue"}}>
+            Made With ❤️ by Abhishek  | <a target="_blank" href="https://www.linkedin.com/in/22rajputabhi/">Contact Developer</a>
+        </div>
+    )
+}
+
+export default Intro
